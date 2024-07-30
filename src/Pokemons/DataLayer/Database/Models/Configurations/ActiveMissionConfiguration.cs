61c19d1e7b70468663d8239fe1e9ae33dd97ae70
Kreuz41 <kreuzework@gmail.com>
2024-07-30 22:45:32 +0300

    default data

--- src/Pokemons/DataLayer/Database/Models/Configurations/ActiveMissionConfiguration.cs
+++ src/Pokemons/DataLayer/Database/Models/Configurations/ActiveMissionConfiguration.cs
@@ -9,5 +9,45 @@ public class ActiveMissionConfiguration : IEntityTypeConfiguration<ActiveMission
     public void Configure(EntityTypeBuilder<ActiveMission> builder)
     {
         builder.HasKey(a => a.Id);
+        builder.Property(a => a.Id).ValueGeneratedOnAdd();
+
+        builder.HasData(new List<ActiveMission>
+        {
+            new()
+            {
+                Id = 1,
+                IsEnded = false,
+                IsDifficult = false,
+                Reward = 50
+            },
+            new()
+            {
+                Id = 2,
+                IsEnded = false,
+                IsDifficult = true,
+                Reward = 50
+            },
+            new()
+            {
+                Id = 3,
+                IsEnded = false,
+                IsDifficult = true,
+                Reward = 50
+            },
+            new()
+            {
+                Id = 4,
+                IsEnded = false,
+                IsDifficult = true,
+                Reward = 50
+            },
+            new()
+            {
+                Id = 5,
+                IsEnded = false,
+                IsDifficult = true,
+                Reward = 50
+            }
+        });
     }
 }
---
+++ src/Pokemons/Migrations/20240730194520_default_data_added.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Pokemons.Migrations
+{
+    /// <inheritdoc />
+    public partial class default_data_added : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "ActiveMissions",
+                columns: new[] { "Id", "EndDate", "IsDifficult", "IsEnded", "Reward" },
+                values: new object[,]
+                {
+                    { 1, null, false, false, 50 },
+                    { 2, null, true, false, 50 },
+                    { 3, null, true, false, 50 },
+                    { 4, null, true, false, 50 },
+                    { 5, null, true, false, 50 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "ActiveMissions",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "ActiveMissions",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "ActiveMissions",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "ActiveMissions",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "ActiveMissions",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
